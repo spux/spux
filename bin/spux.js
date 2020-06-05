8a387cf9249134521cba56287e9ea60f282f4462
Melvin <melvincarvalho@gmail.com>
2020-06-05 10:59:42 +0200

    add view attribute

--- bin/spux.js
+++ bin/spux.js
@@ -3,19 +3,25 @@
 // IMPORTS
 var shell = require('shelljs')
 var fs = require('fs')
+var argv = require('minimist')(process.argv.slice(2))
 
 // MODEL
 globalThis.data = {
-  inputURI: 'package.json'
+  inputURI: 'package.json',
+  view: null
 }
 
 // INIT
-data.inputURI = process.argv[2] || data.inputURI
+data.inputURI = argv._[0] || data.inputURI
+data.view = argv.view || data.view
 
 // MAIN
 // console.error(`reading file ${data.inputURI}`)
 data.input = fs.readFileSync(data.inputURI)
 
-console.log(`<script type="application/ld+json" id="data">
+var viewAttribute = data.view ? ` view="${data.view}"` : ``
+var html = `<script type="application/ld+json" id="data"${viewAttribute}>
 ${data.input.toString()}</script>
-<script type="module" src="https://cdn.jsdelivr.net/gh/spux/spux-shim/web_modules/spux-shim.js"></script>`)
+<script type="module" src="https://cdn.jsdelivr.net/gh/spux/spux-shim/web_modules/spux-shim.js"></script>`
+
+console.log(html)
