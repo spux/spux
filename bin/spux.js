45e4371dfa5d634b98c75e0c6ea76476603456fd
Melvin <melvincarvalho@gmail.com>
2020-06-05 09:56:08 +0200

    add spux bin

---
+++ bin/spux.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+// IMPORTS
+var shell = require('shelljs')
+var fs = require('fs')
+
+// MODEL
+globalThis.data = {
+  inputURI: 'package.json'
+}
+
+// INIT
+data.inputURI = process.argv[2] || data.inputURI
+
+// MAIN
+console.error(`reading file ${data.input}`)
+data.input = fs.readFileSync(data.inputURI)
+
+console.log(`<script type="application/ld+json" id="data">
+${data.input.toString()}</script>
+<script
+  type="module"
+  src="https://cdn.jsdelivr.net/gh/spux/spux-shim/web_modules/spux-shim.js"
+></script>
+`)
