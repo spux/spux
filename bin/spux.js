8ef16b3fec672ba724196b874e3b4c3c32582cc0
Melvin <melvincarvalho@gmail.com>
2020-06-23 10:37:49 +0200

    add shortcut views

--- bin/spux.js
+++ bin/spux.js
@@ -8,12 +8,31 @@ var argv = require('minimist')(process.argv.slice(2))
 // MODEL
 globalThis.data = {
   inputURI: 'package.json',
-  view: null
+  view: null,
+  cdn: 'https://spux.org/modules'
+}
+
+// FUNCTIONS
+function validURL (str) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  ) // fragment locator
+  return !!pattern.test(str)
 }
 
 // INIT
 data.inputURI = argv._[0] || data.inputURI
 data.view = argv.view || data.view
+
+if (data.view && !validURL(data.view)) {
+  data.view = data.cdn + '/' + data.view + '.js'
+}
 
 // MAIN
 // console.error(`reading file ${data.inputURI}`)
