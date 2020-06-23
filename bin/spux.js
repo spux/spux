b501ea89bdd7c61a337c8c2ffdaccbc851242608
Melvin <melvincarvalho@gmail.com>
2020-06-23 12:03:19 +0200

    allow validurl to accept colons

--- bin/spux.js
+++ bin/spux.js
@@ -15,14 +15,9 @@ globalThis.data = {
 // FUNCTIONS
 function validURL (str) {
   var pattern = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
+    '^(https?:\\/\\/)?', // protocol
     'i'
-  ) // fragment locator
+  )
   return !!pattern.test(str)
 }
 
